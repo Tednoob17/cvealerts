5c0de2da9560eb519523a5f5dae6e4dd81d6503b
Ted Kouhouenou <teddams047@gmail.com>
2024-02-26 10:12:06 +0100

    add some file

--- src/cvssmetrics.rs
+++ src/cvssmetrics.rs
@@ -1,7 +1,7 @@
 
 /**
   * struct Reference - 
-  *
+  *  
   *
   *
  */
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 use reqwest::blocking::get;
 use std::env;
-use docx::document::Paragraph;
-use docx::Docx;
-extern crate serde;
-extern crate serde_json;
+use serde_json::Value; // Import the Value type from serde_json
+use std::fs::File;
+use std::io::{Write, Result};
 
-#[derive(Debug, serde::Deserialize)]
-struct {
-
-
-}
-
-fn ft_search_cves(parameters: &str) -> Result<(), reqwest::Error> {
+fn ft_search_cves(parameters: &str) -> Result<()> {
     let base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0";
     let response = get(&format!("{}?{}", base_url, parameters))?;
 
     if response.status().is_success() {
-        let json_data = response.json::<serde_json::Value>()?;
-        println!("{:#}", json_data);
+        let json_data: Value = response.json()?; // Deserialize into serde_json::Value
+        let stdout_content = json_data.to_string(); // Convert to a string
+
+        // Save stdout content to a file
+        save_stdout_to_file(&stdout_content)?;
     } else {
         eprintln!("Erreur lors de la récupération des données. Code d'état : {}", response.status());
     }
@@ -25,6 +21,11 @@ fn ft_search_cves(parameters: &str) -> Result<(), reqwest::Error> {
     Ok(())
 }
 
+fn save_stdout_to_file(stdout_content: &str) -> Result<()> {
+    let mut file = File::create("output.txt")?;
+    file.write_all(stdout_content.as_bytes())?;
+    Ok(())
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,9 +34,4 @@ fn main() {
     if let Err(e) = ft_search_cves(&parameters) {
         eprintln!("Erreur : {}", e);
     }
-
-
-
-
-
 }
